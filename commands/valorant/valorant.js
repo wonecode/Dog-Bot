e0ebf5d70b7bf9b34cc2a392262230ca49418156
Wone <lochlainn.gadault@gmail.com>
2021-08-16 23:51:06 +0200

    try to fix

--- commands/valorant/valorant.js
+++ commands/valorant/valorant.js
@@ -1,9 +1,8 @@
 const { MessageEmbed } = require('discord.js');
 const { Command, CommandoMessage } = require('discord.js-commando');
-const fs = require('fs');
 const cheerio = require('cheerio');
+const request = require('request');
 const got = require('got');
-const axios = require('axios');
 
 module.exports = class valorantCommand extends Command {
   constructor(client) {
@@ -28,21 +27,6 @@ module.exports = class valorantCommand extends Command {
    * @param {string} query
    */
   async run(message, { query }) {
-    return message.say(
-      new MessageEmbed()
-        .setAuthor(
-          'Valorant',
-          'https://studio.cults3d.com/4QqRV9kLYYEuw9ur_X3yjQl1sjk=/516x516/https://files.cults3d.com/uploaders/15024335/illustration-file/a86d53e4-2bd9-4a8f-9550-986686c3131a/gi0mAjIh_400x400.png',
-          'https://playvalorant.com/fr-fr/'
-        )
-        .setColor('ORANGE')
-        .setTitle('Commande en maintenance')
-        .setDescription(
-          "La commande est actuellement indisponible car une maintenance est en cours. Réessayez plus tard."
-        )
-        .setTimestamp()
-    );
-
     const oldQuery = query.split('#');
     let username = oldQuery[0];
     username = username.replace(' ', '');
@@ -55,76 +39,92 @@ module.exports = class valorantCommand extends Command {
       tagline +
       '/overview?playlist=competitive';
 
-    axios
-      .get(vgmUrl, {
+    const req = request.defaults({
+      jar: true, // save cookies to jar
+      rejectUnauthorized: false,
+      followAllRedirects: true, // allow redirections
+    });
+
+    // scrape the page
+    req.get(
+      {
+        url: vgmUrl,
         headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+          'User-Agent': 'Super Cool Browser', // optional headers
         },
-      })
-      .then((response) => {
-        const $ = cheerio.load(response.body);
-        const playerName = $('.trn-ign__username').text();
-        const playerTagline = $('.trn-ign__discriminator').text();
-        const playerIcon = $('image').attr('href');
-        const playerRank = $('.valorant-rank-icon').attr('alt');
+      },
+      function (err, resp, body) {
+        if (resp.statusCode === 200) {
+          // load the html into cheerio
+          const $ = cheerio.load(body);
+          console.log(resp);
+          const playerName = $('.trn-ign__username').text();
+          const playerTagline = $('.trn-ign__discriminator').text();
+          const playerIcon = $('image').attr('href');
+          const playerRank = $('.valorant-rank-icon').attr('alt');
 
-        let playerWinloss = $('text', '.valorant-winloss').text();
-        playerWinloss = playerWinloss.trim();
-        playerWinloss = playerWinloss.split('\n');
+          let playerWinloss = $('text', '.valorant-winloss').text();
+          playerWinloss = playerWinloss.trim();
+          playerWinloss = playerWinloss.split('\n');
 
-        const playerWinsOld = playerWinloss[0];
-        const playerWins = parseInt(playerWinsOld);
-        const playerLossOld = playerWinloss[2].trim();
-        const playerLoss = parseInt(playerLossOld);
-        const playerWinrate = Math.round((playerWins / (playerWins + playerLoss)) * 100) + '%';
+          const playerWinsOld = playerWinloss[0];
+          const playerWins = parseInt(playerWinsOld);
+          const playerLossOld = playerWinloss[2].trim();
+          const playerLoss = parseInt(playerLossOld);
+          const playerWinrate = Math.round((playerWins / (playerWins + playerLoss)) * 100) + '%';
 
-        let giantStats = $('.giant-stats').text();
-        giantStats = giantStats.split('      ');
+          let giantStats = $('.giant-stats').text();
+          giantStats = giantStats.split('      ');
 
-        let headshotPrct = giantStats[2];
-        headshotPrct = headshotPrct.split('  ');
-        const playerHS = headshotPrct[1];
+          let headshotPrct = giantStats[2];
+          headshotPrct = headshotPrct.split('  ');
+          const playerHS = headshotPrct[1];
 
-        let kdRatio = giantStats[1];
-        kdRatio = kdRatio.split('  ');
-        const playerKD = kdRatio[1];
+          let kdRatio = giantStats[1];
+          kdRatio = kdRatio.split('  ');
+          const playerKD = kdRatio[1];
 
-        let agents = $('.agent').text();
-        agents = agents.split('  ');
-        const playerMainAgent = agents[1].trim();
+          let agents = $('.agent').text();
+          agents = agents.split('  ');
+          const playerMainAgent = agents[1].trim();
 
-        const playerMainWeapon = $('.weapon__name').html();
+          const playerMainWeapon = $('.weapon__name').html();
 
-        return message.say(
-          new MessageEmbed()
-            .setAuthor(playerName + ' ' + playerTagline, playerIcon, vgmUrl)
-            .setColor('RED')
-            .addField('Rang', playerRank, true)
-            .addField('Winrate', `${playerWinrate} (${playerWins}V/${playerLoss}D)`, true)
-            .addField('Headshot %', playerHS, true)
-            .addField('K/D Ratio', playerKD, true)
-            .addField('Main agent', playerMainAgent, true)
-            .addField('Team', playerMainWeapon, true)
-            .setFooter($('h2').text())
-        );
-      })
-      .catch((err) => {
-        console.log(err);
+          return message.say(
+            new MessageEmbed()
+              .setAuthor(playerName + ' ' + playerTagline, playerIcon, vgmUrl)
+              .setColor('RED')
+              .addField('Rang', playerRank, true)
+              .addField('Winrate', `${playerWinrate} (${playerWins}V/${playerLoss}D)`, true)
+              .addField('Headshot %', playerHS, true)
+              .addField('K/D Ratio', playerKD, true)
+              .addField('Main agent', playerMainAgent, true)
+              .addField('Team', playerMainWeapon, true)
+              .setFooter($('h2').text())
+          );
+        } else {
+          return message.say(
+            new MessageEmbed()
+              .setAuthor(
+                'Valorant',
+                'https://studio.cults3d.com/4QqRV9kLYYEuw9ur_X3yjQl1sjk=/516x516/https://files.cults3d.com/uploaders/15024335/illustration-file/a86d53e4-2bd9-4a8f-9550-986686c3131a/gi0mAjIh_400x400.png',
+                'https://playvalorant.com/fr-fr/'
+              )
+              .setTitle('Joueur introuvable')
+              .setDescription(
+                "Le joueur que tu as recherché est introuvable ou n'a pas connecté son compte avec tracker.gg (https://tracker.gg/valorant/)"
+              )
+              .setTimestamp()
+          );
+        }
+      }
+    );
 
-        return message.say(
-          new MessageEmbed()
-            .setAuthor(
-              'Valorant',
-              'https://studio.cults3d.com/4QqRV9kLYYEuw9ur_X3yjQl1sjk=/516x516/https://files.cults3d.com/uploaders/15024335/illustration-file/a86d53e4-2bd9-4a8f-9550-986686c3131a/gi0mAjIh_400x400.png',
-              'https://playvalorant.com/fr-fr/'
-            )
-            .setTitle('Joueur introuvable')
-            .setDescription(
-              "Le joueur que tu as recherché est introuvable ou n'a pas connecté son compte avec tracker.gg (https://tracker.gg/valorant/)"
-            )
-            .setTimestamp()
-        );
-      });
+    return message.say(
+      new MessageEmbed()
+        .setTitle('Recherche :hourglass_flowing_sand:')
+        .setDescription('Recherche du joueur en cours...')
+        .setColor('GREEN')
+    );
   }
 };
